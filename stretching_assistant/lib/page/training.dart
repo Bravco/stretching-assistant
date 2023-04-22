3f686eff3b0fdc5cb828cdc2b284dcebfdba44cd
Bravco <boss.wiw@gmail.com>
2023-04-22 09:19:35 +0200

    Added exercises list view to each training

--- stretching_assistant/lib/page/training.dart
+++ stretching_assistant/lib/page/training.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stretching_assistant/utils.dart';
 
 // Model
 import 'package:stretching_assistant/model/training.dart';
@@ -20,11 +21,65 @@ class TrainingPage extends StatelessWidget {
       child: Scaffold(
         extendBodyBehindAppBar: true,
         appBar: AppBar(),
-        body: TrainingPreview(
-          training: training,
-          width: MediaQuery.of(context).size.width,
-          height: 160,
-          borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(8), bottomRight: Radius.circular(8)),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              TrainingPreview(
+                training: training,
+                width: MediaQuery.of(context).size.width,
+                height: 160,
+                borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(8), bottomRight: Radius.circular(8)),
+              ),
+
+              for (int i = 0; i < training.exercises.length; i++) Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Utils.secondaryBackgroundColor,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              training.exercises[i].key.name,
+                              style: TextStyle(
+                                color: Utils.textColorAlt,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            Text(
+                              Utils.formatSeconds(training.exercises[i].value.inSeconds),
+                              style: const TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Image(
+                          height: 96,
+                          image: training.exercises[i].key.image,
+                          fit: BoxFit.cover,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.play_arrow),
         ),
       ),
     );
